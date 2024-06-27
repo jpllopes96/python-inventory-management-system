7bdc964f1a1866aa2c3a185c1cd01910170d0212
jpllopes96 <joaolacaia96@gmail.com>
2024-06-27 10:56:08 -0300

    Brand Details

--- brands/views.py
+++ brands/views.py
@@ -1,5 +1,6 @@
-from django.views.generic import ListView
-from . import models
+from django.views.generic import ListView, CreateView, DetailView
+from django.urls import reverse_lazy
+from . import models, forms
 
 
 class BrandListView(ListView):
@@ -16,3 +17,16 @@ class BrandListView(ListView):
             queryset = queryset.filter(name__icontains=name)
 
         return queryset
+
+
+class BrandCreateView(CreateView):
+    model = models.Brand
+    template_name = 'brand_create.html'
+    form_class = forms.BrandForm
+    success_url = reverse_lazy('brand_list')
+
+
+class BrandDetailView(DetailView):
+    model = models.Brand
+    template_name = 'brand_detail.html'
+    context_object_name = 'brand'
