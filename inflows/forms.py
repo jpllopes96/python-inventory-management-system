f4f099f61c74fe5fb2c3a54596c0f0365f324cea
jpllopes96 <joaolacaia96@gmail.com>
2024-07-01 11:36:09 -0300

    ADMIN for Models

--- inflows/forms.py
+++ inflows/forms.py
@@ -10,6 +10,6 @@ class InflowForm(forms.ModelForm):
         widgets = {
             'supplier': forms.Select(attrs={'class': 'form-control'}),
             'product': forms.Select(attrs={'class': 'form-control'}),
-            'quantity': forms.TextInput(attrs={'class': 'form-control'}),
+            'quantity': forms.NumberInput(attrs={'class': 'form-control'}),
             'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 3})
         }
