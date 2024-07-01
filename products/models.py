1585c05aecb0969af05c02d6c57b4db9860b5669
jpllopes96 <joaolacaia96@gmail.com>
2024-07-01 16:02:24 -0300

    Product CRUD

--- products/models.py
+++ products/models.py
@@ -10,9 +10,9 @@ class Product(models.Model):
     description = models.TextField(null=True, blank=True)
     serie_number = models.CharField(max_length=200, null=True, blank=True)
     cost_price = models.DecimalField(max_digits=20, decimal_places=2)
-    salling_price = models.DecimalField(max_digits=20, decimal_places=2)
+    selling_price = models.DecimalField(max_digits=20, decimal_places=2)
     quantity = models.IntegerField(default=0)
-    created_at = models.DateTimeField(auto_now_add=True),
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
