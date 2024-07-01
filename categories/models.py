a989e5d238e230da6b62f8a88011901717019d4e
jpllopes96 <joaolacaia96@gmail.com>
2024-07-01 10:27:08 -0300

    CRUD for Category

--- categories/models.py
+++ categories/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=500)
-    description = models.TextField(null=True, blank=True),
-    created_at = models.DateTimeField(auto_now_add=True),
+    description = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
