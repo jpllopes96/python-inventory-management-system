8b13568f73e07b0e1d9e1c4b7db19ae31c4b3426
jpllopes96 <joaolacaia96@gmail.com>
2024-07-03 13:40:18 -0300

    Inflow and Outflow signals to calculate inventory

---
+++ outflows/signals.py
@@ -0,0 +1,12 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from . import models
+
+
+@receiver(post_save, sender=models.Outflow)
+def update_product_quantity(sender, instance, created, **kwargs):
+    if created:
+        if instance.quantity > 0:
+            product = instance.product
+            product.quantity -= instance.quantity
+            product.save()
